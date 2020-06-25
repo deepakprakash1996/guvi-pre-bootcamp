ec525283d5e04f1665c88ba19600a0c46735eaa7
Deepak Prakash <67250327+deepakprakash1996@users.noreply.github.com>
2020-06-25 14:30:53 +0530

    Task upload

---
+++ Task2.1.js
@@ -0,0 +1,15 @@
+//task2.1 SQR of a number
+var a=2;
+console.log(a*a);
+
+//task2.2 BattingAvg
+var virat =100, dhoni=75, rohit=50, bumra=20, avg;
+avg = (virat+dhoni+rohit+bumra)/4;
+console.log(avg);
+
+//task2.3
+//Printing a # Table
+var array = [];
+for(var i=0; i<7; i++)
+{ array.push('#');
+  console.log(array.join(" "));  }
---
+++ task1.1.js
@@ -0,0 +1,31 @@
+//task1.1
+var a,b,c,d;
+console.log(a,b,c,d);
+
+//task1.2
+var myvar=5;
+console.log(myvar);
+
+//task1.3
+var fname = "Deepak";
+var lname ="Prakash";
+var mstatus="Single";
+var country = "India";
+var age ="24";
+
+//task1.4
+var fname="Deepak", lname ="Prakash", mstatus="Single", country = "India", age ="24";
+
+//task1.5
+var string ="String";
+var boolean = true;
+var a =5;
+var b= null;
+console.log(string,boolean,a,b);
+
+//task1.6
+var a = "22";
+var b = parseInt(a);
+var c = Number(a);
+var d = +a;
+console.log(typeof(a),typeof(b),typeof(c),typeof(d));
